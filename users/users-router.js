e8c2d385c49a42d5f1daf96b7f7afe19554739af
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-07-10 15:13:41 -0700

    setup router

--- users/users-router.js
+++ users/users-router.js
@@ -5,4 +5,64 @@ const restrict = require("../middleware/restrict");
 
 const router = express.Router();
 
+router.get("/users", restrict(), async (req, res, next) => {
+  try {
+    res.json(await Users.find());
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/users", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await Users.findBy({ username }).first();
+
+    if (user) {
+      return res.status(409).json({
+        message: "Username is already taken",
+      });
+    }
+
+    const newUser = await Users.add({
+      username,
+
+      password: await bcrypt.hash(password, 16),
+    });
+
+    res.status(201).json(newUser);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await Users.findBy({ username }).first();
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid Credentials",
+      });
+    }
+
+    const passwordValid = await bcrypt.compare(password, user.password);
+
+    if (!passwordValid) {
+      return res.status(401).json({
+        message: "Invalid Credentials",
+      });
+    }
+
+    req.session.user = user;
+
+    res.json({
+      message: `Welcome ${user.username}!`,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
